c07b951e361ecb12f6e0b7a638bb3d8869046883
DaeYoun <leedyoun6@gmail.com>
2018-12-19 01:21:21 +0900

    0.1.0.1
    	push가 안되서 방황하는 중이다

--- src/components/Editor/EditorPane/EditorPane.js
+++ src/components/Editor/EditorPane/EditorPane.js
@@ -3,10 +3,26 @@ import React, {Component} from 'react';
 import styles from './EditorPane.scss';
 import classNames from 'classnames/bind';
 
+import CodeMirror from 'codemirror';
+
+import 'codemirror/mode/markdown/markdown';
+// 마크 다운 문법 색생, 마크다운 내부에 들어가는 코드 색상
+
+import 'codemirror/mode/javascript/javascriprt';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+
+// ComdeMirror을 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
 const cx = classNames.bind(styles);
 
 
 class EditorPane extends Component {
+
+  editor = null // 에디터
 
   render () {
 
